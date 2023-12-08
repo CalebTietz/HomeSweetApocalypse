3805b9ee99efd8a83c1ae46c0424a5d7a1f0484b
lts7602 <73794602+lts7602@users.noreply.github.com>
2023-12-08 15:28:27 -0600

    HighScore

--- Home Sweet Apocalypse/Assets/Scripts/HighScore.cs
+++ Home Sweet Apocalypse/Assets/Scripts/HighScore.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class HighScore : MonoBehaviour
 {
+    private TMP_Text uiText; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        uiText=GetComponent<TMP_Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        uiText.text = "Highscore is: "+ ScoreKeeper.highScore.ToString("#,0");
     }
 }
